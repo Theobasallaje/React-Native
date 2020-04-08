b931b2c0db2534ee76d55e32e013cf3c523935b3
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2020-04-07 21:52:59 -0500

    added stopwatch container

--- Stopwatch/App.js
+++ Stopwatch/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import StopWatchContainer from './stopwatch/stopwatch.container';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey Austin</Text>
+      <Text style={styles.title}>Hey Austin!</Text>
+      <StopWatchContainer />
     </View>
   );
 }
@@ -12,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    display: "flex",
+    backgroundColor: '#DBD053',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: "8%",
   },
+
+  title: {
+    fontSize: 30,
+    color: "#74526C",
+    marginBottom: "8%"
+  }
+
 });
